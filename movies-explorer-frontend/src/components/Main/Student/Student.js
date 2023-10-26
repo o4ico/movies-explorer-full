b27b1667177aa60faecf56bc9454d0824bdd973a
Darya Sokolova <o4ico@yandex.ru>
2023-10-26 12:28:20 +0300

    feat: добавить инфо о себе и фото

--- movies-explorer-frontend/src/components/Main/Student/Student.js
+++ movies-explorer-frontend/src/components/Main/Student/Student.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Student.css';
 
 import Article from '../Article/Article';
-import studentPhoto from '../../../images/portfolio.png';
+import studentPhoto from '../../../images/portfolio.jpg';
 import { Link } from 'react-router-dom';
 
 function Student() {
@@ -11,10 +11,9 @@ function Student() {
       title='Студент' classContainer={'article_student'}>
 
       <section className="student">
-        <h3 className="student__name">Виталий</h3>
-        <p className="student__about">Фронтенд-разработчик, 30 лет</p>
-        <p className="student__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить. С&nbsp;2015 года работал в компании «СКБ Контур».
-          После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+        <h3 className="student__name">Дарья</h3>
+        <p className="student__about">Фронтенд-разработчик, 23 года</p>
+        <p className="student__text">Я родилась и живу в Волгограде, отучилась на&nbsp;кафедре «Дизайн и&nbsp;технология моды» ВТК по&nbsp;специальности «Технолог-конструктор». Попутно училась заочно на рекламе. Я&nbsp;люблю рисовать, закончила художественную школу с&nbsp;отличием. Не так давно заинтересовалась IT-сферой, так и&nbsp;начала кодить. Прошла курс по веб-разработке, с&nbsp;нуля освоив новую профессию.
         </p>
         <img className="student__photo" src={studentPhoto} alt="фото для портфолио"></img>
         <Link className="student__link" to="https://github.com/o4ico" target='_blank'>Github</Link>
